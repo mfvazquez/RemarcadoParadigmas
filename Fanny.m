635de6526161e388b27a8ee3d5288aab90de9154
mfvazquez <mfvazquezfiuba@gmail.com>
2016-05-17 10:44:04 -0300

    terminado el remarcado del fanny random d1

--- Fanny.m
+++ Fanny.m
@@ -13,123 +13,79 @@ addpath('iEEG_toolbox');
 eeglab
 
 CHANNEL = 45;
-
 edf_file = fullfile('..','Paciente14_Fagioli','MI_d1_Random.edf');
-% EEG = pop_biosig(edf_file);
 EEG=import_edf(edf_file,CHANNEL);
-
-eeglab redraw
-  
-% ev2delete=[1:6];
-%  
-% EEG=delete_mark(EEG,ev2delete)
-
 eeglab redraw
 
 %% Leo los tiempos de las marcas del fotodiodo
 
 micromed_time=make_photodiodevector(EEG);
-% 
-micromed_time=micromed_time(1, 2:end);
-micromed_time=micromed_time - micromed_time(1,4);
+ev2delete=[1 2 110 length(micromed_time)];
+EEG=delete_mark(EEG,ev2delete);
+micromed_time=make_photodiodevector(EEG);
+micromed_time = micromed_time - micromed_time(1);
 
-% ---------------- LOG ---------------------------------------------------
+
+%% ---------------- LOG ---------------------------------------------------
 
 log_file=fullfile('log', 'Fanny', '14_D1', '114RandomSequenceBlock1.mat');
 load(log_file);
-
 stimTrigSentTime = stimTrigSentTime(stimTrigSentTime~=0);
 stimTrigSentTime = stimTrigSentTime - stimTrigSentTime(1,1);
-
-
-
-%% Primera prueba
-
-% stimuli_time=stimTrigSentTime+0.15; %0.183
-% stimuli_time=stimuli_time(1:720, 1);
-% tr=keyPressTime+0.15;
-% tr=keyPressTime(1:720, 1);
+stimTrigSentTime = stimTrigSentTime + micromed_time(1,3);
 
 %%
-
 plot([micromed_time;micromed_time],[-100;0],'r')
 hold on   
 plot([stimTrigSentTime';stimTrigSentTime'],[0;100],'b')
 
-% hold on
-% plot([tr';tr'],[0;100],'g')
-%    
-
-%%
-
-% %% Elimino las marcas que están de más
-
-EEG=pop_editeventvals(EEG,'delete',[1 2 3 4]);
-        
  %%
 
-for i=1:max(size(stimuli_time))
+for i=1:max(size(stimTrigSentTime))
   
-        rest=micromed_time-stimuli_time(i); 
+        rest=micromed_time-stimTrigSentTime(i); 
         mmark=find(abs(rest)<0.05);
         
         if ~isempty(mmark)
             
-                EEG = pop_editeventvals(EEG,'changefield',{mmark 'type' 'S'});
+                EEG = pop_editeventvals(EEG,'changefield',{mmark 'type' 'Stim'});
                         
         end
         
 end
 
-%%
+for i = 1:10
 
-% %% Elimino las marcas que están de más
-
-p=1;
-
-while p<size(EEG.event,2)
-   
-    if strcmp(EEG.event(p).type,'chan8') == 1
-        
-        EEG=delete_mark(EEG,p);
-        
-    else
-        
-        p=p+1;
-        
-    end
+    mmark_random = 1+12*(i-1);
+    
+    EEG = pop_editeventvals(EEG,'changefield',{mmark_random 'type' 'Random'});
+    EEG = pop_editeventvals(EEG,'changefield',{mmark_random+1 'type' 'Move'});
     
 end
 
+
 %% Agrego las respuestas
-
-i=1;
-
-tr=tr-stimuli_time(1,1);
-
-a=length(EEG.event)+length(tr);
-
-while i<a
-
-    b=find(tr>(EEG.event(i).latency-EEG.event(1).latency)/EEG.srate & tr<(EEG.event(i+1).latency-EEG.event(1).latency)/EEG.srate);
-        
-    for j=1:length(b)
-        EEG = pop_editeventvals(EEG,'append',{i [] [] []},'changefield',{i+1 'latency'  EEG.event(1).latency/EEG.srate+tr(b(j))},'changefield',{i+1 'type' 'R'});
-        i=i+1;
+shift = 0;
+n = 0;
+for i = 1:length(EEG.event)
+    i
+    if strcmp(EEG.event(i+shift).type, 'Stim')
+        n = n+1
+        if (reactionTime(n) ~= 0)
+            event_time = EEG.event(i+shift).latency/EEG.srate
+            value = event_time + reactionTime(n)
+            EEG = pop_editeventvals(EEG,'add',{i+shift value});
+            EEG = pop_editeventvals(EEG,'changefield',{i+1+shift 'type' 'Reaction'});
+            shift = shift + 1
+        end
     end
-    i=i+1;
 end
-%%
 
-clear micromed_time
-figure
-micromed_time=make_photodiodevector(EEG);
-micromed_time=micromed_time-micromed_time(1,1);
+%% GUARDO EL SET
 
-stimuli_time=stimuli_time-stimuli_time(1,1);
+set_path = fullfile('..','ConMarcas','Fanny','Paciente14');
 
-plot([micromed_time;micromed_time],[-100;0],'r')
-hold on   
+EEG = eeg_checkset( EEG );
+EEG = pop_saveset( EEG, 'filename','MI_d1_Random.set','filepath',set_path);
+[ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
 
-plot([stimuli_time';stimuli_time'],[0;100],'b')
-hold on   
